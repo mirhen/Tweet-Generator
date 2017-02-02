f3e0590fdfa54a4437d9b05ee67aab7a71d68888
Miriam Hendler <mjammer18@gmail.com>
2017-02-02 13:08:30 -0800

    updates made

--- Online Academy/analyze.py
+++ Online Academy/analyze.py
@@ -1,24 +1,32 @@
-def histogram():
-    graph = {'one': 1}
-    words = 'one fish two fish red fish blue fish'
-    list1 = words.split()  #this is your original list of words
-    list2 = []   #this is a new list
 
-    for word in list1:
-        old = word
+def histogram(filename):
+    content = []
+    list2 = {}  #this is a new list
+    with open(filename,'r') as f:
+        for line in f:
+            for word in line.split():
+                content.append(word)
+
+    for word in content:
         if word in list2:
-            list2.index(word)[1] += 1
+            list2[word] += 1
         else:
-            list2.append({word:0})
+            list2[word] = 1
 
-        print list2
+    return list2
 
-def unique_words():
-
-    return 0
-def frequency(str):
-    return 0
+def unique_words(hist):
+    count = 0
+    for word in hist:
+        if hist[word] == 1:
+            count += 1
+    return count
+def frequency(hist, string):
+    return hist[string]
 
 
 if __name__ == '__main__':
-    histogram()
+    hist = histogram('madeline.txt')
+    count = unique_words(hist)
+    freq = frequency(hist, 'the')
+    print count, freq
--- Online Academy/dictionary_words.py
+++ Online Academy/dictionary_words.py
@@ -1,29 +1,33 @@
 import random
 import sys
 
-text = ""
-num = 0
-
-with open('/usr/share/dict/words') as textfile:
-    content = textfile.readlines()
-# you may also want to remove whitespace characters like `\n` at the end of each line
-content = [x.strip() for x in content]
-
 def random_python_word():
 
     rand_index = random.randint(0, len(content) - 1)
     return content[rand_index]
 
-if sys.argv[1:][0].isdigit():
-    num = int(sys.argv[1:][0])
-    if num > len(content) - 1:
-        print 'that number is too big'
-        num = 0
-else:
-    print 'You have to input a number'
+def retrieve_words():
+    with open('/usr/share/dict/words') as textfile:
+        content = textfile.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    content = [x.strip() for x in content]
 
-for i in range(num):
-    word = random_python_word()
-    text += " " + word
+    text = ""
 
-print text
+    if sys.argv[1:][0].isdigit():
+        num = int(sys.argv[1:][0])
+        if num > len(content) - 1:
+            print 'that number is too big'
+            num = 0
+    else:
+        print 'You have to input a number'
+
+    for i in range(num):
+        word = random_python_word()
+        text += " " + word
+
+    print text
+
+
+if __name__ == '__main__':
+    retrieve_words()
